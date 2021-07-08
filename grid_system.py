129de6a7c569aa03118cea630457f8720ba68868
ArmandoBolanios <mordana_93@hotmail.com>
2021-07-08 14:51:00 -0500

    Se modificó el archivo grid_system.py

--- grid_system.py
+++ grid_system.py
@@ -7,6 +7,7 @@ myLabel1 = Label(root, text="Hello World")
 myLabel2 = Label(root, text="My name is Hermann")
 
 myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+myLabel2.grid(row=1, column=5)
+
 
 root.mainloop()
