c4d608d7836284f4bf901d78af02c7115ed94048
ArmandoBolanios <mordana_93@hotmail.com>
2021-07-08 14:40:53 -0500

    Se subió archivo de grid

---
+++ grid_system.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+#creating a label widget
+myLabel1 = Label(root, text="Hello World")
+myLabel2 = Label(root, text="My name is Hermann")
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
+
+root.mainloop()
