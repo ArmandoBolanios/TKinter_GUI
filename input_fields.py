8ed4ddb1383c20dc41bf734a86420424ab260d43
ArmandoBolanios <mordana_93@hotmail.com>
2021-07-09 14:16:30 -0500

    Modificación del archivo

--- input_fields.py
+++ input_fields.py
@@ -2,15 +2,16 @@ from tkinter import *
 
 root = Tk()
 
-e = Entry(root, width=50, borderwidth=5)
+e = Entry(root, width=50)
 e.pack()
-
+e.insert(0, "Enter your Name: ")
 
 def myClick():
-    myLabel = Label(root, text=e.get())
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
     
-myButton = Button(root, text="Enter your name", command=myClick)    
+myButton = Button(root, text="Enter your stock quote", command=myClick)    
 myButton.pack()
 
 root.mainloop()
